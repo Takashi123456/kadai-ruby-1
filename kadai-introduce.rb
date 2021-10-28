604503c5c797bfd2ce5c445623a8e8399b32ef8f
takashi.sato <weshop2wen@gmail.com>
2021-10-28 13:02:29 +0000

    thrd commit kadai-introduce.rb

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,5 +1,5 @@
-secondName = '佐藤'
-firstName = '隆'
+second_name = '佐藤'
+first_name = '隆'
 age = 35
 
-puts secondName + firstName + 'です。'+age.to_s + '歳です。'
+puts second_name + first_name + 'です。'+ age.to_s + '歳です。'
